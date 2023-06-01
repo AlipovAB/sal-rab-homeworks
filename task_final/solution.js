e3cdafdf88405d7502edc0e0f6b7dfbb3466f9a9
AlipovAB <Alipov_A_B@mail.ru>
2023-06-01 15:43:41 +0300

    task_final

--- task_final/solution.js
+++ task_final/solution.js
@@ -34,20 +34,29 @@
 // }
 
 function sendRequest(name, phone, address, goods, sum) {
-    let data = {goods: [], order: {}};
+    
+    let data = {
+        client: '', 
+        order: {},
+        goods: [] 
+        }
 
     let countOfGoods = goods.length;
 
-    for (let i = 0; i <= countOfGoods; i += 1) {
-        data.goods.push(goods[i].title);
+    for (let i = 0; i < countOfGoods; i += 1) {
+        data.goods.push({title:goods[i].title, count:goods[i].count});
     }
 
-    data.order.address = address;
-    data.order.sum = name + phone + address + goods + sum;
+    data.order.address = "ул. " + address.street + ", дом " + address.house + ", " + address.entrance + " подъезд, " + address.floor + " этаж, кв. " + address.flat;
+   // data.order.address = address;
+    console.log(data.order.address);
 
-    data.client = 'Иван';
+    data.order.sum = sum;
+    console.log(data.order.sum);
 
-    let jsonData = JSON.stringify(data);
+    data.client = name + ' ' + phone;
 
+    let jsonData = JSON.stringify({data});
+    
     return jsonData;
 }
